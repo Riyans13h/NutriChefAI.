a9ccc984b5417fcac420729fff700195db8bfca8
Riyans13h <riyanshsachan1@gmail.com>
2025-12-14 18:06:26 +0530

    Update __init__.py

---
+++ ai_engine/embeddings/__init__.py
@@ -0,0 +1,9 @@
+"""
+embeddings package
+
+Responsible for generating vector embeddings
+for recipes and user queries.
+
+These embeddings are stored in the vector database
+and used for semantic retrieval in the RAG pipeline.
+"""
