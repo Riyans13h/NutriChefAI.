d40170f1a9425e76745a84bc6b6f50d0c8e8ef7e
Riyansh Sachan <riyanshsachan1@gmail.com>
2025-12-14 23:29:42 +0530

    back

---
+++ ai_engine/vector_store/__init__.py
@@ -0,0 +1,9 @@
+"""
+chroma_db package
+
+This package contains ChromaDB-specific configurations
+and storage directories used for vector embeddings.
+
+Acts as the persistence layer for semantic search
+in the RAG pipeline.
+"""
